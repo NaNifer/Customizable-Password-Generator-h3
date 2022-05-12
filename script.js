f97b241f1433684e394016df9627a22faae141b3
Nifer Kilakila <nifercritter@gmail.com>
2022-05-11 23:21:23 -0700

    edited psuedocode and created working dialog boxes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Added global varitables so that they can be accessed outside of dialogBoxes()
+var chosenNums;
+var lowerCaseAsk;
+var upperCaseAsk;
+var numericAsk;
+var specialAsk;
+
 // Assignment Code -- generateBtn = "generate password" button
 var generateBtn = document.querySelector("#generate");
-
-
 
 // dialog buttons parameters
 // - 8 characters and no more than 128 characters
@@ -12,22 +17,18 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", dialogBoxes);
 
 function dialogBoxes() {
-  let askNums1 = prompt ("Please enter how many characters you would like your password to be. It can be between 8 characters and no more than 128 characters.", "Enter a number, IE. '8, 12, 111'");
+  let askNums1 = prompt("Please enter how many characters you would like your password to be. It can be between 8 characters and no more than 128 characters.", "Enter a number, IE. '8, 12, 111'");
 
-  if (Number(askNums1) === NaN ) {
-    let askNums2 = prompt ("Please enter a number between 8 - 128.", "Enter a number.");
-      if (Number(askNums2) === NaN ) { 
-        // Set up loop
-      }
-      else {
-        let chosenNums = Number(askNums2);
-      }
+  if (Number(askNums1) !== NaN && Number(askNums1) >= 8 && Number(askNums1) <= 128) {
+    chosenNums = Number(askNums1)
   }
   else {
-    let chosenNums = Number(askNums1);
+    let askNums2 = prompt("Please enter a number between 8 - 128.", "Enter a number.");
+    while (Number(askNums2) === NaN || Number(askNums2) < 8 || Number(askNums2) > 128) {
+      askNums2 = prompt("Please enter a number between 8 - 128.", "Enter a number.");
+    }
+    chosenNums = Number(askNums2);
   }
-}
-
 let lowerCaseAsk = confirm("Would you like to include lowercase characters?");
 
 let upperCaseAsk = confirm("Would you like to include uppercase characters?");
@@ -35,14 +36,15 @@ let upperCaseAsk = confirm("Would you like to include uppercase characters?");
 let numericAsk = confirm("Would you like to include numeric characters?  IE., 0-9 ");
 
 let specialAsk = confirm("Would you like to include special characters?  IE., $ & @ !");
+}
 
+function writePassword() {
 
+}
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-console.log(askNums1)
-console.log(askNums2 + "two")
-console.log(lowerCaseAsk)
-
-
+  passwordText.value = password;
 
 
 
@@ -58,15 +60,16 @@ console.log(lowerCaseAsk)
 
 
 
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
 
 // }
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
+
+
+
+// Extra dialog box if I have time:
+// let parameterSummary = confirm("The criteria that you chose for your password are as follows: \nThe password will be " + chosenNums + " charaters long.\n" + "It will utilize these characters" + specialAsk )
 
